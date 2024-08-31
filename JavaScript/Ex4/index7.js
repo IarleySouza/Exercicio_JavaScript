6e96594d6623fef23c2f2744663fa57c4e380813
iarley <iarleysouza615@gmail.com>
2024-08-31 15:01:33 -0300

    Exercicios adicionados

---
+++ JavaScript/Ex4/index7.js
@@ -0,0 +1,6 @@
+var velocidade = 78.5
+console.log(`A velocidade do seu carro é ${velocidade}Km/h`)
+if (velocidade > 60) { //Condição simples
+    console.log(`Você ultrapassou o limete de velocidade. MULTADO`)
+}
+console.log(`Dirija sempre usando o cinto de segurança!`)
