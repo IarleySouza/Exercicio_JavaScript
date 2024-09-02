f51818d4afc1a96c9a409bb37cf0896ec588aa3b
iarley <iarleysouza615@gmail.com>
2024-09-02 01:21:46 -0300

    Exercício adicionado

--- JavaScript/Ex6-ex/ex07/script.js
+++ JavaScript/Ex6-ex/ex07/script.js
@@ -1,5 +1,13 @@
-var cont = 1
-
-while (cont <=10) {
-
+function tabuada() {
+    var cont = 0
+    var nu = document.getElementById('numero')
+    var res = document.getElementById('res')
+    while (cont <= 10) {
+        var tab = cont * Number(nu.value)
+        res.innerHTML = `${nu.value} x ${cont} = ${tab}`
+        cont++
+        
+    }
+    
+    
 }
